488113d17407b969bfc507f8b8eba1348cf84693
Evan Miller <miller.evan815@gmail.com>
2016-01-25 15:13:33 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Steam-AppManifestor
+
+Download Steam games unavailable to your platform through `appmanifest` trickery.
+
+This **should** work on all platforms, although only OS X has been tested even partially.
+
+Based on [this repo](https://github.com/dotfloat/steam-appmanifest).
+
+## Usage
+
+Find the AppID for the app you want to download.
+These IDs can be found here: http://steamdb.info/
+
+//TODO: installation procedure
+
+The program will walk you through the following steps:
+
+- AppID: Just enter the AppID you found on http://steamdb.info/.
+
+- Where would you like to output the appmanifest?
+This will give you four choices:
+  1. Your SteamApps folder (detected based on platform, may not be correct)
+  2. Your Desktop
+  3. Other (will prompt you to type in a directory path)
+  4. stdout (prints the resulting file to the terminal)
+
+The other options are really not very useful, as the appmanifest must be in your
+SteamApps folder to do any good. Once you have the file (named properly) in your
+SteamApps folder, simply restart steam and the game should begin to download.
+
+## Potential Issues
+
+If your operating system creates your SteamApps folder someplace weird,
+create an issue and let me know! If you're having a different problem,
+create an issue and let me know! If you have an idea for a feature...
+you get the idea.
+
+## ISC License
+
+Copyright (c) 2016 Evan Miller
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
---
+++ index.js
@@ -0,0 +1,81 @@
+#! /usr/bin/env node
+'use strict'
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
+
+let steamapps = ''
+switch(process.platform){
+  case 'win32': steamapps = 'C:/Program Files/Valve/Steam/SteamApps'; break
+  case 'darwin': steamapps = os.homedir()+'/Library/Application Support/Steam/steamapps'; break
+  default: steamapps = os.homedir()+'/.steam/steam/SteamApps'; break;
+}
+
+console.log(chalk.bold('Please enter the AppID of the app you would like to download.'))
+console.log(chalk.red.bold('This only works for apps on your account!!!'))
+console.log('AppIDs can be found here: '+chalk.green('http://steamdb.info/'))
+
+inquirer.prompt([
+  {
+    'type': 'input',
+    'name': 'appid',
+    'message': 'AppID:',
+    'validate': input=>{
+      if(isNaN(+input)){
+        return "That's not a number!"
+      }else{
+        return true
+      }
+    }
+  },
+  {
+    'type': 'list',
+    'name': 'out',
+    'message': 'Where would you like to output the appmanifest?',
+    'choices': [
+      steamapps,
+      os.homedir()+'/Desktop',
+      'Other',
+      'stdout'
+    ]
+  },
+  {
+    'when': answers=>answers.out==='Other',
+    'type': 'input',
+    'name': 'dir',
+    'message': 'Output Dir:',
+    'validate': function(input){
+      let done = this.async()
+      fs.stat(input.replace('~', os.homedir()), (err, stats)=>{
+        if(err){
+          done(err)
+        }else if(!stats.isDirectory()){
+          done("That isn't a directory, try again!")
+        }else{
+          done(true)
+        }
+      })
+    }
+  }
+], answers=>{
+  let manifest = `
+  "AppState"
+    {
+      "AppID"  "${answers.appid}"
+      "Universe" "1"
+      "StateFlags" "1026"
+    }
+  `
+  if(answers.out==='stdout'){
+    console.log(manifest)
+  }else{
+    let filepath = path.join(answers.dir.replace('~', os.homedir()), 'appmanifest_'+answers.appid+'.acf')
+    fs.writeSync(filepath, manifest)
+    console.log('File has been written to '+chalk.yellow(filepath))
+    if(answers.out===steamapps){
+      console.log(chalk.purple.bold('Restart Steam')+'. The app download should begin immediately.')
+    }
+  }
+})
